74bddf5f5915c597bde44ad848316a90f8fb6634
hiroto09 <k23015kk@aitech.ac.jp>
2025-05-07 13:39:54 +0900

    update:Sectionの実装

---
+++ src/components/layout/Section/route.test.tsx
@@ -0,0 +1,14 @@
+import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Section from ".";
+
+describe("初期レンダリング時のテスト", () => {
+    test("Section", () => {
+        render(<Section title="都道府県" />);
+        expect(screen.getByText("都道府県")).toBeInTheDocument();
+        render(<Section title="都道府県別人口" />);
+        expect(screen.getByText("都道府県別人口")).toBeInTheDocument();
+    });
+});
+
